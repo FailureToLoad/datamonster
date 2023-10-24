330ffb74c7dd1dba6b73101c77d4c0506cc1758b
Dave.M <failuretoload@github>
2023-10-24 13:27:01 -0700

    expands settlement request layer

--- datamonster.web/src/api/api.ts
+++ datamonster.web/src/api/api.ts
@@ -12,15 +12,30 @@ export type Settlement = {
     departing: number
 }
 
+export type CreateSettlementRequest = {
+    settlementName: string
+    userId: string
+}
+
 type api = {
+  getSettlementsForUser: (userId: string) => Promise<Settlement[]>
   getSettlement: () => Promise<Settlement>
+  createSettlement: (request: CreateSettlementRequest) => Promise<Settlement>
     
 }
 
 const requester:api = {
+  getSettlementsForUser: async function (userId: string): Promise<Settlement[]> {
+      const response = await ax.get<Settlement[]>(`http://localhost:8000/settlement`,{headers: {Authorization: userId}})
+      return response.data
+  },
   getSettlement: async function (): Promise<Settlement> {
       const response = await ax.get<Settlement>('http://localhost:8000/settlement')
       return response.data
+  },
+  createSettlement: async function (request: CreateSettlementRequest): Promise<Settlement> {
+    const response = await ax.post<Settlement>('http://localhost:8000/settlement', request)
+    return response.data
   }
 }
 
