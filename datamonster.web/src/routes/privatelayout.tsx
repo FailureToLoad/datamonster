ad646faf5f21ebcc43e422af40b9b6ec7d222e8e
Dave M. <failuretoload@github>
2023-11-14 14:44:26 -0700

    replaces loadng text with spinner

--- datamonster.web/src/routes/privatelayout.tsx
+++ datamonster.web/src/routes/privatelayout.tsx
@@ -1,10 +1,10 @@
 import { AuthContext } from "@/auth/auth-context";
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-
+import Spinner from "@/components/spinner";
 function PrivateLayout() {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <Spinner />;
   if (!isLoggedIn) return <Navigate to="/login" />;
   return <Outlet />;
 }
