ffe99984ff029f590e82289595b346de5d0a1226
Dave M. <failuretoload@github>
2023-11-16 11:45:42 -0700

    implements simpler header until more is needed

--- datamonster.web/src/routes/settlement/index.tsx
+++ datamonster.web/src/routes/settlement/index.tsx
@@ -13,39 +13,23 @@ interface HeaderProps {
 function Header({ settlement }: HeaderProps) {
   return (
     <div id="header" className="sticky top-0 w-full flex-none">
-      <div className="m-2 flex h-20">
-        <div className="flex items-center">
-          <div className="mx-2 border-2 border-solid border-black px-4 py-2 text-lg font-semibold">
-            {settlement?.limit}
-          </div>
-          <div>Survival Limit</div>
-        </div>
-        <Separator
-          className="m-4 w-[1px] shrink-0 bg-black"
-          orientation="vertical"
-        />
-        <div className="flex w-2/6 flex-col items-start justify-center">
+      <div className="m-2 flex h-20 justify-center">
+        <div className="flex w-1/3 flex-col">
           <div className="flex w-full">
-            <h1 className="w-auto flex-none font-medium">Settlement Name</h1>
             <div className="inline-flex grow justify-center">
               {settlement?.name}
             </div>
           </div>
-          <Separator className="my-1 h-[2px] w-full shrink-0 bg-black " />
-          <div>
-            <NavigationMenu>
-              <NavigationMenuList>
-                <Link to="timeline/" className={navigationMenuTriggerStyle()}>
-                  Timeline
-                </Link>
-                <Link to="population/" className={navigationMenuTriggerStyle()}>
-                  Population
-                </Link>
-                <Link to="storage/" className={navigationMenuTriggerStyle()}>
-                  Storage
-                </Link>
-              </NavigationMenuList>
-            </NavigationMenu>
+          <div className="inline-flex justify-center">
+            <Link to="timeline/" className={navigationMenuTriggerStyle()}>
+              Timeline
+            </Link>
+            <Link to="population/" className={navigationMenuTriggerStyle()}>
+              Population
+            </Link>
+            <Link to="storage/" className={navigationMenuTriggerStyle()}>
+              Storage
+            </Link>
           </div>
         </div>
       </div>
@@ -64,7 +48,9 @@ function Settlement() {
   return (
     <div className="flex h-screen w-full flex-col">
       <Header settlement={settlement as Settlement} />
-      <Outlet />
+      <div className="p-16">
+        <Outlet />
+      </div>
     </div>
   );
 }
