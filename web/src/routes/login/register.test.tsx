4b53606c7d3dbb77142307def73bc9d6a75d184d
Dave M. <failuretoload@github>
2023-12-16 18:53:37 -0700

    Adds register form submission test

--- web/src/routes/login/register.test.tsx
+++ web/src/routes/login/register.test.tsx
@@ -1,9 +1,10 @@
 import { expect, it, describe, vi, beforeEach } from "vitest";
 import { RegisterDialogue } from "./register";
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import userApi from "@/api/user";
 import { setInterceptor } from "@/api/api";
+import { act } from "react-dom/test-utils";
 const user = {
   userId: 1,
   token: "token",
@@ -29,5 +30,32 @@ describe("Register", () => {
     expect(passwordField).toBeInTheDocument();
     const confirmPasswordField = screen.getByLabelText(/confirm password/i);
     expect(confirmPasswordField).toBeInTheDocument();
+  });
+  it("submits the register form", async () => {
+    vi.mocked(userApi).register = mockRegister;
+    vi.mocked(setInterceptor).mockImplementation = vi.fn();
+    render(build());
+    const registerButton = screen.getByRole("button", { name: "Register" });
+    expect(registerButton).toBeInTheDocument();
+    await userEvent.click(registerButton);
+
+    const usernameField = screen.getByLabelText("Username");
+    expect(usernameField).toBeInTheDocument();
+    await userEvent.click(usernameField);
+    await userEvent.paste("username");
+
+    const passwordField = screen.getByLabelText("Password");
+    expect(passwordField).toBeInTheDocument();
+    await userEvent.click(passwordField);
+    await userEvent.paste("password");
+
+    const confirmPasswordField = screen.getByLabelText(/confirm password/i);
+    expect(confirmPasswordField).toBeInTheDocument();
+    await userEvent.click(confirmPasswordField);
+    await userEvent.paste("password");
+    const submitButton = screen.getByRole("button", { name: "Submit" });
+    expect(submitButton).toBeInTheDocument();
+    await userEvent.click(submitButton);
+    expect(mockRegister).toHaveBeenCalledWith("username", "password");
   });
 });
