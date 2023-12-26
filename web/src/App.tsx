1c36a79d747b3adbca0b456ad36a4d54358fabd7
Dave M. <failuretoload@github>
2023-12-26 15:39:37 -0700

    refactor settlement index to just handle exports

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,7 +10,7 @@ import Spinner from "./components/spinner.tsx";
 import Login, { LoginAction, LoginLoader } from "./routes/login/login.tsx";
 import { Selector, SettlementListLoader } from "./routes/settlementSelector";
 
-import Settlement, { SettlementLoader } from "./routes/settlement";
+import { Settlement, SettlementLoader } from "./routes/settlement";
 import Timeline from "./routes/settlement/timeline.tsx";
 import Population from "./routes/settlement/population/index.tsx";
 import SettlementStorage from "./routes/settlement/settlementStorage.tsx";
