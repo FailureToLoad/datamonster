fb032d7f148cf55506d475d0c214976401334cc2
Dave Mason <davejmason@outlook.com>
2024-06-26 21:15:00 -0700

    drops go backend

--- api/web/mocks/routeGuardMock.go
+++
@@ -1,9 +0,0 @@
-package mocks
-
-import "net/http"
-
-func RouteGuardMock(handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r)
-	}
-}
