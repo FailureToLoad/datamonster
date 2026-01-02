88800bd3be72ef3cfa81aebfb5f1d36a58c60f62
FailureToLoad <31629598+FailureToLoad@users.noreply.github.com>
2026-01-02 00:42:29 -0700

    Drops ag-grid from chunking

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ["react", "react-dom", "react-router"],
-          grid: ["ag-grid-community", "ag-grid-react"],
         },
       },
     },
