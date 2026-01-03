597b15d1484606534c493419681154c109d9ccca
FailureToLoad <31629598+FailureToLoad@users.noreply.github.com>
2026-01-03 13:11:37 -0700

    Adds context for static game data

--- web/src/components/DefaultLayout.tsx
+++ web/src/components/DefaultLayout.tsx
@@ -1,10 +1,13 @@
 import {Outlet} from 'react-router';
 import styles from './ProtectedLayout.module.css';
+import {GlossaryProvider} from '~/context/glossary';
 
 export default function Layout() {
     return (
-        <div className={styles.layout}>
-            <Outlet />
-        </div>
+        <GlossaryProvider>
+            <div className={styles.layout}>
+                <Outlet />
+            </div>
+        </GlossaryProvider>
     );
 }
